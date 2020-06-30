d491805927f91e7bcc45ceb9a1552af8e4dc9008
SharifAmit <sharifamit@iub.edu.bd>
2020-06-29 21:15:47 -0700

    plot laplace sobel

--- denoising_pipeline.py
+++ denoising_pipeline.py
@@ -7,41 +7,51 @@ from scipy import ndimage
 import argparse
 import time
 import os
+import warnings
+warnings.filterwarnings("ignore")
 
-
-def sum1dsignal(img,img_name):
+def sum1dsignal(img,img_name,plot):
     np_img = np.asarray(img) 
-    np_sum = np.sum(np_img, axis=1) 
-    plt.plot(np_sum[:,0])
-    filename = 'Outputs/sum_signal1d/'+img_name+'.png'
-    plt.savefig(filename)
-    plt.close()
+    np_sum = np.sum(np_img, axis=1)
+    if plot==True:
+        plt.plot(np_sum[:,0])
+        filename = 'Outputs/sum_signal1d/'+img_name+'.png'
+        plt.savefig(filename)
+        plt.close()
     return np_sum
 
-def gaussain1d(sum1d,sigma,img_name):
+def gaussain1d(sum1d,sigma,img_name,plot):
     gauss_img = ndimage.filters.gaussian_filter1d(sum1d[:,0],sigma)
-    plt.plot(gauss_img)
-    filename = 'Outputs/gaussian_1d/'+img_name+'.png'
-    plt.savefig(filename)
-    plt.close()
+    if plot==True:
+        plt.plot(gauss_img)
+        filename = 'Outputs/gaussian_1d/'+img_name+'.png'
+        plt.savefig(filename)
+        plt.close()
     return gauss_img
 
-def gradient2ndOrder(gauss_img,img_name):
-    grad_img = np.gradient(gauss_img,2)
-    plt.plot(grad_img)
-    filename = 'Outputs/gradient_2ndOrder/'+img_name+'.png'
-    plt.savefig(filename)
-    plt.close()
+def gradient2ndOrder(gauss_img,img_name,filter,plot):
+    if filter=='sobel':
+        grad_img = ndimage.sobel(gauss_img,axis=0,mode='constant')
+    elif filter=='laplacian':
+        grad_img = np.gradient(gauss_img,2)
+    if plot==True:
+        plt.plot(grad_img)
+        filename = 'Outputs/gradient_2ndOrder/'+img_name+'.png'
+        plt.savefig(filename)
+        plt.close()
     return grad_img
 
-def zeroCrossing(grad_img,img_name):
+def zeroCrossing(grad_img,img_name,plot):
     #img_no_neg = np.where(grad_img!=0,grad_img,-500)
     #img_no_neg = np.where(img_no_neg<0.5,-500,0)
-    img_no_neg = np.where((grad_img<0.5) & (grad_img>=0),-500,0)
-    plt.plot(img_no_neg)
-    filename = 'Outputs/zero_crossing/'+img_name+'.png'
-    plt.savefig(filename)
-    plt.close()
+    min_beta= min(grad_img)
+    min_beta = min_beta+0.5
+    img_no_neg = np.where((grad_img<=min_beta) & (grad_img>=0),-500,0)
+    if plot==True:
+        plt.plot(img_no_neg)
+        filename = 'Outputs/zero_crossing/'+img_name+'.png'
+        plt.savefig(filename)
+        plt.close()
     return img_no_neg
 
 def denoisedImage(img,img_no_neg,img_name):
@@ -125,7 +135,9 @@ if __name__ == "__main__":
     parser.add_argument('--enhance',type=int, required=False, help='size of enhance filter', default=3)
     parser.add_argument('--SOL',type=bool, required=False, help='streak of light required', default=False)
     parser.add_argument('--threshold_SOL', type=int, required=False, help='threhsold for streak of light', default=65)
-    
+    parser.add_argument('--gradient_filter',type=str, required=False, help='Either Laplacian or Sobel', default='laplacian')
+    parser.add_argument('--plot_signals',type=bool, required=False, help='Plot the signals for sum,gaussian,gradient,zero_crossings', default=False)
+
     args = parser.parse_args()
     start_time = time.time()
     dir_names = ['Outputs','Outputs/sum_signal1d','Outputs/gaussian_1d','Outputs/gradient_2ndOrder','Outputs/zero_crossing','Outputs/denoised_image','Outputs/enhanced_image','Outputs/median_image','Outputs/enhanced_change_image','Outputs/median_image2','Outputs/streak_of_light']
@@ -136,11 +148,11 @@ if __name__ == "__main__":
         img_name, _ = i.split('.')
         filepath = args.dir+'/'+i
         img = Image.open(filepath)
-        np_sum = sum1dsignal(img,img_name)
+        np_sum = sum1dsignal(img,img_name,args.plot_signals)
         sigma = 30
-        gauss_img = gaussain1d(np_sum,sigma,img_name)
-        grad_img = gradient2ndOrder(gauss_img,img_name)
-        img_no_neg = zeroCrossing(grad_img,img_name)
+        gauss_img = gaussain1d(np_sum,sigma,img_name,args.plot_signals)
+        grad_img = gradient2ndOrder(gauss_img,img_name,args.gradient_filter,args.plot_signals)
+        img_no_neg = zeroCrossing(grad_img,img_name,args.plot_signals)
         im_np_img = denoisedImage(img,img_no_neg,img_name)
         enhanced_img = enhanched(im_np_img,args.enhance,img_name)
         enhanced_changed_img = median_subtracting_img(enhanced_img,args.small_median,img_name)
